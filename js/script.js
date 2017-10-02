9a983cdee9bd7131c28e11e532f34e2af0f95f62
joe mckinney <mckinney.a.joe@gmail.com>
2017-10-02 11:48:48 -0700

    add js so that the user input is sorted through and only words containing three letters or more are added to the new array

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
   $("form#sentence").submit(function(event) {
     var sentence = $("#words").val();
-    var words = sentence.split();
-    console.log(words);
+    var words = sentence.split(" ");
+    var newArray = [];
+    var input = words.map(function(word) {
+      if (word.length > 3) {
+        newArray.push(word);
+      }
+    });
+
+    alert(newArray);
 
 
 
