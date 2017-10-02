109d9fb9a04b392652c7110f009f247611e7348d
joe mckinney <mckinney.a.joe@gmail.com>
2017-10-02 12:22:39 -0700

    clean up the code in script.js to look nicer

--- js/script.js
+++ js/script.js
@@ -3,23 +3,18 @@ $(document).ready(function() {
     var sentence = $("#words").val();
     var words = sentence.split(" ");
     var newArray = [];
+
     var input = words.map(function(word) {
       if (word.length > 3) {
         newArray.push(word);
       }
     });
+
     var newArrayReverse = newArray.reverse().join();
-    $("#output").text(newArrayReverse);
+    $("#output").text(newArrayReverse );
     $("#init-showing").hide();
     $("#init-hiding").show();
 
-
-
-    console.log(newArrayReverse);
-
-
-
-
-    event.preventDefault();
+  event.preventDefault();
   });
 });
